056b5568642f7a3ce0af6451a12b9f76402a8ea5
Yacine <yacineaoumara2@DESKTOP-21GP5J2>
2023-04-16 23:26:03 +0100

    nn

---
+++ VsProject/Models/PatientModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace VsProject.Models
+{
+    internal class PatientModel : INotifyPropertyChanged
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string gender;
+        private string phoneNumber;
+        private string emailAddress;
+        public ObservableCollection<PatientModel> Patients { get; set; }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(nameof(Id)); }
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+
+        [Required]
+        [Range(0, 120)]
+        public int Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged(nameof(Age)); }
+        }
+
+        [Required]
+        [StringLength(10)]
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; OnPropertyChanged(nameof(Gender)); }
+        }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; OnPropertyChanged(nameof(PhoneNumber)); }
+        }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { emailAddress = value; OnPropertyChanged(nameof(EmailAddress)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ VsProject/ViewModels/PatientViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VsProject.Models;
+
+namespace VsProject.ViewModels
+{
+    internal class PatientViewModel : INotifyPropertyChanged
+
+    {
+        public PatientViewModel() {
+                Patients = new ObservableCollection<PatientModel>();
+                LoadPatients();            
+        }
+        private int _id;
+        private string _name;
+        private int _age;
+        private string _gender;
+        private string _phoneNumber;
+
+        public ObservableCollection<PatientModel> Patients { get; set; }
+
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (_age != value)
+                {
+                    _age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+
+        public string Gender
+        {
+            get { return _gender; }
+            set
+            {
+                if (_gender != value)
+                {
+                    _gender = value;
+                    OnPropertyChanged(nameof(Gender));
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (_phoneNumber != value)
+                {
+                    _phoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void LoadPatients()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Patients", connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    PatientModel patient = new PatientModel
+                    {
+                        Id = (int)reader["Id"],
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        Age = (int)reader["Age"],
+                        Gender = reader["Gender"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString(),
+                        EmailAddress = reader["EmailAddress"].ToString()
+                    };
+                    Patients.Add(patient);
+                }
+                reader.Close();
+            }
+        }
+
+
+    }
+}
