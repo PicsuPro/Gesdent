5a5cb4fe118ea8820bbcc2bd5d950153bd9b4c63
Picsu <Picsu@DESKTOP-8AI23J0>
2023-06-01 10:06:24 +0200

    yes

--- VsProject/App.xaml.cs
+++ VsProject/App.xaml.cs
@@ -22,15 +22,15 @@ namespace VsProject
             VMVMappings.Initialize();
             CultureInfo.CurrentCulture = new CultureInfo("fr-FR");
             var mainView = new MainView();
-            //if ((bool)DialogService.Show(new LoginViewModel()))
-            //{
-            UserPrincipal.SetUser(new NetworkCredential("yes", "yes"));
+            if ((bool)DialogService.Show(new LoginViewModel()))
+            {
+                //UserPrincipal.SetUser(new NetworkCredential("yes", "yes"));
                 mainView.Show();
-            //}
-            //else
-            //{
-            //    Shutdown();
-            //}
+            }
+            else
+            {
+                Shutdown();
+            }
         }
     }
 }
